8b89be18903de3bd76f3e50d9907adb95d6fe170
github username <ronaldcuello11@gmail.com>
2018-10-25 13:29:36 -0400

    Randomize box colors

--- src/index.js
+++ src/index.js
@@ -2,25 +2,28 @@ document.addEventListener('DOMContentLoaded', () => {
    const comments = document.getElementById('comments');
    const comments_right = document.getElementById('comments_right');
    const endPoint = 'https://country.register.gov.uk/records.json?page-size=5000';
+   const boxColorArray = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan", ,"DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"]
+
    const symbol = [];
 
    // fetch the names of the countries
-function firstRound(){
-  fetch(`https://country.register.gov.uk/records.json?page-size=5000`)
-    .then(r => r.json())
-    .then(data => {
-      const info = Object.values(data)
-        const element = info[Math.floor(Math.random() * info.length)];
-          const li_tag = document.createElement('div')
-            li_tag.style["width"] =  "300px"
-            li_tag.style["height"]=  "50px"
-            li_tag.style["margin"]=  "1em"
-            li_tag.style["background-color"] = "red"
-            li_tag.innerText = element.item[0].name
-            li_tag.id = element.item[0].country
-            li_tag.class = "countries"
-            li_tag.dataset.countryName = element.item[0].name
-            comments.append(li_tag)
+  function firstRound(){
+    fetch(`https://country.register.gov.uk/records.json?page-size=5000`)
+      .then(r => r.json())
+      .then(data => {
+        const info = Object.values(data)
+          const element = info[Math.floor(Math.random() * info.length)];
+          const color = boxColorArray[Math.floor(Math.random() * boxColorArray.length)];
+            const li_tag = document.createElement('div')
+              li_tag.style["width"] =  "300px"
+              li_tag.style["height"]=  "50px"
+              li_tag.style["margin"]=  "1em"
+              li_tag.style["background-color"] = `${color}`
+              li_tag.innerText = element.item[0].name
+              li_tag.id = element.item[0].country
+              li_tag.class = "countries"
+              li_tag.dataset.countryName = element.item[0].name
+              comments.append(li_tag)
   })}
 
   comments.addEventListener('click', function(){
@@ -73,18 +76,17 @@ function firstRound(){
   comments_right.addEventListener('submit', function(event){
     event.preventDefault()
       let y = event.target[0]
-      console.log(y)
-        if (y.value == y.className){
-          console.log(y)
-          alert("Great answer!")
-          firstRound()
-        }
-      else{
-        event.target.reset()
-      }
+        console.log(y)
+          if (y.value == y.className){
+              alert("Great answer!")
+                firstRound()
+          }
+          else{
+            event.target.reset()
+          }
   })
 }
 
-firstRound()
+  firstRound()
 })
 //
