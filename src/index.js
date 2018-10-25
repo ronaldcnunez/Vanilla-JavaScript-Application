6e8b3d8e91433adfdb7487792783d13cd60d0bfd
github username <ronaldcuello11@gmail.com>
2018-10-25 12:44:59 -0400

    Css styling

--- src/index.js
+++ src/index.js
@@ -4,22 +4,25 @@ document.addEventListener('DOMContentLoaded', () => {
    const endPoint = 'https://country.register.gov.uk/records.json?page-size=5000';
    const symbol = [];
 
-
    // fetch the names of the countries
 function firstRound(){
   fetch(`https://country.register.gov.uk/records.json?page-size=5000`)
     .then(r => r.json())
-      .then(data => {
-        const info = Object.values(data)
-          console.log(info)
-            const element = info[Math.floor(Math.random() * info.length)];
-              const li_tag = document.createElement('div')
-                li_tag.innerText = element.item[0].name
-                li_tag.id = element.item[0].country
-                li_tag.class = "countries"
-                li_tag.dataset.countryName = element.item[0].name
-                 comments.append(li_tag)
+    .then(data => {
+      const info = Object.values(data)
+        const element = info[Math.floor(Math.random() * info.length)];
+          const li_tag = document.createElement('div')
+            li_tag.style["width"] =  "300px"
+            li_tag.style["height"]=  "50px"
+            li_tag.style["margin"]=  "1em"
+            li_tag.style["background-color"] = "red"
+            li_tag.innerText = element.item[0].name
+            li_tag.id = element.item[0].country
+            li_tag.class = "countries"
+            li_tag.dataset.countryName = element.item[0].name
+            comments.append(li_tag)
   })}
+
   comments.addEventListener('click', function(){
    let x = event.target
       const formHTML =`<form id="comment_form">
@@ -47,11 +50,15 @@ function firstRound(){
           const xtraInfo = Object.values(data)
           const element = xtraInfo[Math.floor(Math.random() * xtraInfo.length)];
           const li_tag = document.createElement('div')
-           li_tag.innerText = element.item[0]["name"]
-           li_tag.id = element.item[0]["citizen-names"]
-           li_tag.class = "countries"
-           li_tag.dataset.countryName = element.item[0].name
-            comments_right.append(li_tag)
+            li_tag.style["width"] =  "300px"
+            li_tag.style["height"]=  "50px"
+            li_tag.style["margin"]=  "1em"
+            li_tag.style["background-color"] = "blue"
+              li_tag.innerText = element.item[0]["name"]
+              li_tag.id = element.item[0]["citizen-names"]
+              li_tag.class = "countries"
+              li_tag.dataset.countryName = element.item[0].name
+              comments_right.append(li_tag)
         })
 
   comments_right.addEventListener('click', function(){
